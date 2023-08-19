d800b374f0cdcb4bf18908ad8e8c42ecf2133ac0
Issenn <28648060+issenn@users.noreply.github.com>
2023-08-19 22:31:36 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -118,10 +118,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          ChatGPT Next
+          神经管理学与人工智能实验室
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          经理人个性化数字智能孪生: Build MyGPT
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
