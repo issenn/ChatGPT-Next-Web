0007801492a520267f31d399760aa38982835888
Issenn <28648060+issenn@users.noreply.github.com>
2023-08-19 22:30:32 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "ChatGPT Next Web",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "神经管理学与人工智能实验室",
+  description: "经理人个性化数字智能孪生: Build MyGPT",
   viewport: {
     width: "device-width",
     initialScale: 1,
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "神经管理学与人工智能实验室",
     statusBarStyle: "default",
   },
 };
